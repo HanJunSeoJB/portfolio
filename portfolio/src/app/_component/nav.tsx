f718f00040037c083811cfe7bb91b22d8180973f
HanJunseoJB <hjs123@jbnu.ac.kr>
2025-05-31 20:18:33 +0900

    feat: 네비 바 호버 기능 추가

--- portfolio/src/app/_component/nav.tsx
+++ portfolio/src/app/_component/nav.tsx
@@ -7,13 +7,13 @@ type NavProps = {
 
 function Nav({ userName }: NavProps) {
     return (
-        <div className={styles.inner}>  
+        <div className={styles.inner}>    
           <span className={styles.name}>{userName}</span>
           <div className={styles.nav}>
-            <Link href ='/home'>Home</Link>
-            <Link href ='/about'>About</Link>
-            <Link href ='/project'>Projects</Link>
-            <Link href = '/archiving'>Archiving</Link>
+            <div className={styles.paths}><Link href ='/home'>Home</Link></div>
+            <div className={styles.paths}><Link href ='/about'>About</Link></div>
+            <div className={styles.paths}><Link href ='/project'>Projects</Link></div>
+            <div className={styles.paths}><Link href = '/archiving'>Archiving</Link></div>
           </div>
         </div>
     )
