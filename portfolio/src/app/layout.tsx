a41b48b12048460c73bb9f10a03521d02fbfc678
HanJunSeoJB <hjs123@jbnu.ac.kr>
2025-06-04 21:50:24 +0900

    fix: 안쓰는 폰트 삭제제

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,26 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Noto_Sans} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
-
-const notoSans = Noto_Sans({
-  variable: "--font-notoSans",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
