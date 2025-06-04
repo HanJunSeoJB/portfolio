883915427369f9f731637851f449c766af479ed7
HanJunseoJB <hjs123@jbnu.ac.kr>
2025-06-04 18:09:06 +0900

    feat: 이력서 버튼 제작 중, 프로젝트 보기 버튼 제작 완료

---
+++ portfolio/src/app/_component/job.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { useEffect, useState } from "react";
+import style from "../_style/profile.module.css"
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+    subsets: ["latin"],
+})
+
+const messages = [
+    "프론트엔드 개발자",
+    "데이터 분석가",
+]
+
+export default function Job() {
+    const [index, setIndex] = useState(0);
+    
+        useEffect( () => {
+            const timer = setInterval(()=> {
+                setIndex((prev) => (prev + 1) % messages.length);
+            }, 3000);
+    
+            return () => clearInterval(timer);
+        }, []);
+    return (
+        <div>
+            <h3 className={`${style.dev} ${notoSans.className}`}>{messages[index]}</h3>
+        </div>
+    )
+}
