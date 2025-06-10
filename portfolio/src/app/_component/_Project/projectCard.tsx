b4dd42d497aa5834fbfd9970d9219d932f65a07e
HanJunseoJB <hjs123@jbnu.ac.kr>
2025-06-10 11:24:05 +0900

    feat: 프로젝트 카드 생성 중

--- portfolio/src/app/_component/_Project/projectCard.tsx
+++ portfolio/src/app/_component/_Project/projectCard.tsx
@@ -2,6 +2,11 @@ import style from "../../_style/_project/project_card.module.css"
 import Image from "next/image"
 import projectImage_1 from "../../../../public/images/projects/project_1.png"
 import { Noto_Sans } from "next/font/google"
+// import { FaArrowRightLong } from "react-icons/fa6";
+import { IoMdArrowForward } from "react-icons/io";
+
+
+
 
 const notoSans = Noto_Sans({
     subsets: ["latin"],
@@ -21,7 +26,7 @@ export default function ProjectCard() {
             <div className={style.cardHeader}>
                 <h3 className={notoSans.className}>포트폴리오 웹사이트</h3>
                 <p>2025.05 ~ 현재</p>
-                <p>포트폴리오 웹사이트</p>
+                <p>Next js와 몽고db를 이용한 사용자 맞춤형<br/>포트폴리오 웹 사이트입니다!</p>
             </div>
             <div className={style.note}>
                 <div className={style.stack}>
@@ -30,6 +35,23 @@ export default function ProjectCard() {
                     <span className={notoSans.className}>Mongo DB</span>
                     <span className={notoSans.className}>Vercel</span>
                 </div>
+                <div className={style.description}>
+                    <div className={style.descriptionHeader}>
+                        <IoMdArrowForward className={style.arrow}/>
+                        <p>전북대 재학생 인증 API를 구현해서 <br/>재학생 인증 기능을 만들었습니다.</p>
+                    </div>
+                    <div className={style.descriptionHeader}>
+                        <IoMdArrowForward className={style.arrow}/>
+                        <p>컴포넌트를 체계적으로 분리하고, DB에서 사용자 정보를 불러와 포트폴리오를 구성할 수 있도록 구현했습니다.</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className={style.buttons}>
+                <a href="/projects" className={style.buttonLink}>
+                    자세히 보기
+                    <IoMdArrowForward className={style.buttonIcon}/>
+                </a>
             </div>
         </div>
     )
